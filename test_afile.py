6ca00dfea045806e06098c5a6620315f251dd921
Georgios Rizothanasis <trivialgeorge@gmail.com>
2018-04-02 22:25:59 +0200

    Remove bar function and change test names to only foo

--- test_afile.py
+++ test_afile.py
@@ -2,14 +2,14 @@ import unittest
 
 from afile import foo
 
-class TestA(unittest.TestCase):
-    def test_foo_avg(self):
+class TestFoo(unittest.TestCase):
+    def test_avg(self):
         self.assertEquals(10, foo(5))
 
-    def test_foo_zero(self):
+    def test_zero(self):
         self.assertEquals(0, foo(0))
         
-    def test_foo_negative(self):
+    def test_negative(self):
         self.assertEquals(-10, foo(-5))
 
 if __name__ == "__main__":
