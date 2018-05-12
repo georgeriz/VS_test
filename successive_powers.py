0fea225adc1bbca765f874c1f02f13e3dc0d1648
Georgios Rizothanasis <trivialgeorge@gmail.com>
2018-05-12 23:02:14 +0200

    added my solution as well

--- successive_powers.py
+++ successive_powers.py
@@ -1,4 +1,4 @@
-def last_digit(lst):
+def last_digit_best(lst):
     if not lst:
         return 1
     else:
@@ -14,4 +14,35 @@ def last_digit(lst):
             print "#3", n, out
     return lst[0]**out% 10
 
-print last_digit([7, 6, 21])
+def foo(lst):
+    if len(lst) == 1:
+        return lst[0]%4
+    if lst[0]%4 == 3:
+        if lst[1]%2 == 0:
+            return 1
+        return 3
+    elif lst[0]%4 == 2:
+        if lst[1] == 1:
+            return 2
+        return 0
+    return lst[0]%4 # 0 or 1
+
+def last_digit(lst):
+    if len(lst) == 0:
+        return 1
+    if len(lst) == 1:
+        return lst[0]%10
+    for i in range(len(lst) - 1, 0, -1):
+        if lst[i] == 0:
+            lst[i-1] = 1
+    lasts = [[0]*4,
+            [1]*4,
+            [6,2,4,8],
+            [1,3,9,7],
+            [6,4]*2,
+            [5]*4,
+            [6]*4,
+            [1,7,9,3],
+            [6,8,4,2],
+            [1,9]*2]
+    return lasts[lst[0]%10][foo(lst[1:])]
