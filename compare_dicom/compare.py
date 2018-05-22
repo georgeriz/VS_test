bf91d1f2536de2820e05c43696a0c8093c6d00f5
Georgios Rizothanasis <trivialgeorge@gmail.com>
2018-05-22 23:08:43 +0200

    project compare: change to function

--- compare_dicom/compare.py
+++ compare_dicom/compare.py
@@ -1,2 +1,3 @@
-with open('example') as f:
-    d = {line.split()[0]: line.split('[')[1].split(']')[0] for line in f}
+def get_dict(filename):
+    with open(filename) as f:
+        return {line.split()[0]: line.split('[')[1].split(']')[0] for line in f}
