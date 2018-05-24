6b8266cec46c6b5501e8a7b20b6cbd50373c02e7
Georgios Rizothanasis <trivialgeorge@gmail.com>
2018-05-25 00:03:23 +0200

    handles only plurals

--- duration/duration.py
+++ duration/duration.py
@@ -1,2 +1,14 @@
 def duration(seconds):
-	pass
+	m, s = divmod(seconds, 60)
+	h = m/60
+	d = h/24
+	y = d/365
+	res = [y,d,h,m,s]
+	q = ["year", "day", "hour", "minute", "second"]
+	t = [str(i) + " " + q[j] for j,i in enumerate(res) if i != 0]
+	t = [i + "s" for i in t if i!=1]
+	f = ", ".join(t[:-1])
+	if f:
+		return f + " and " + t[-1]
+	return t[-1]
+	
--- duration/test_duration.py
+++ duration/test_duration.py
@@ -4,9 +4,15 @@ from duration import duration
 class TestDuration(unittest.TestCase):
 	def test_seconds(self):
 		self.assertEquals(duration(2), "2 seconds")
-		
+
+	def test_one_second(self):
+		self.assertEquals(duration(1), "1 second")
+
 	def test_minutes(self):
 		self.assertEquals(duration(122),"2 minutes and 2 seconds")
+
+	def test_one_minute(self):
+		self.assertEquals(duration(60), "1 minute")
 		
 		
 if __name__ == "__main__":
