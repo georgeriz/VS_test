f89e243af89e753bdc4e1c4a88e86167986499fc
Georgios Rizothanasis <trivialgeorge@gmail.com>
2018-05-27 21:36:48 +0200

    project duration: replace for loop with map

--- duration/duration.py
+++ duration/duration.py
@@ -6,8 +6,6 @@ def duration(seconds):
 	y = p%365
 	t = ["year", "day", "hour", "minute", "second"]
 	t = [str(j) + " " + t[i] for i,j in enumerate((y,d,h,m,s)) if j != 0]
-	for i in range(len(t)):
-		if not t[i].startswith("1"):
-			t[i] += "s"
+	t = map(lambda x: x if x.startswith("1") else x + "s", t)
 	return ", ".join(t[:-1]) + " and " + t[-1] if len(t) > 1 else t[0]
 	
