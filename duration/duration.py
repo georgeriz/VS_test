f18877b3f80717697bd3c8b186e3a24ebb957f03
Georgios Rizothanasis <trivialgeorge@gmail.com>
2018-05-27 21:35:30 +0200

    project duration: add plural

--- duration/duration.py
+++ duration/duration.py
@@ -6,6 +6,8 @@ def duration(seconds):
 	y = p%365
 	t = ["year", "day", "hour", "minute", "second"]
 	t = [str(j) + " " + t[i] for i,j in enumerate((y,d,h,m,s)) if j != 0]
-	t = [i + "s" for i in t if not i.startswith("1")]
+	for i in range(len(t)):
+		if not t[i].startswith("1"):
+			t[i] += "s"
 	return ", ".join(t[:-1]) + " and " + t[-1] if len(t) > 1 else t[0]
 	
