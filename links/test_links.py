2be0fd09d15d05aa9b9567388557e75dc241d962
Georgios Rizothanasis <trivialgeorge@gmail.com>
2018-06-05 21:49:02 +0200

    project extract links: use assertEqual since order matters and fix test name

--- links/test_links.py
+++ links/test_links.py
@@ -3,25 +3,25 @@ from extract import foo, bar
 
 class TestFoo(unittest.TestCase):
     def test_one_link(self):
-        self.assertItemsEqual(foo("this is bug-123"), ["bug-123"])
+        self.assertEqual(foo("this is bug-123"), ["bug-123"])
     
     def test_two_links(self):
-        self.assertItemsEqual(foo("bug-34 and bug-423 need fixing"), ["bug-34", "bug-423"])
+        self.assertEqual(foo("bug-34 and bug-423 need fixing"), ["bug-34", "bug-423"])
 
     def test_punctuation_after_link(self):
-        self.assertItemsEqual(foo("fix bug-3243."), ["bug-3243"])
+        self.assertEqual(foo("fix bug-3243."), ["bug-3243"])
 
     def test_no_link(self):
-        self.assertItemsEqual(foo("hello world"), [])
+        self.assertEqual(foo("hello world"), [])
 
     def test_almost_link_but_no(self):
-        self.assertItemsEqual(foo("this bug-verification"), [])
+        self.assertEqual(foo("this bug-verification"), [])
 
 class TestBar(unittest.TestCase):
     def test_one_link(self):
         self.assertEqual(bar("this is bug-123"), "Did you mean http://jira.com/bug-123 ?")
 
-    def test_two_links(self):
+    def test_multiple_links(self):
         self.assertEqual(bar("bug-34, bug-1200 and bug-423 need fixing"), 
                             "Did you mean http://jira.com/bug-34 and http://jira.com/bug-1200 and http://jira.com/bug-423 ?")
 
