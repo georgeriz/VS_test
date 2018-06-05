224ca1b319b1fa1a8da780944f3342087aaca366
Georgios Rizothanasis <trivialgeorge@gmail.com>
2018-06-05 20:36:08 +0200

    extract links: tests skeleton

---
+++ links/test_links.py
@@ -0,0 +1,21 @@
+import unittest
+from extract import foo
+
+class TestFoo(unittest.TestCase):
+    def test_one_link(self):
+        pass
+    
+    def test_two_links(self):
+        pass
+
+    def test_punctuation_after_link(self):
+        pass
+
+    def test_no_link(self):
+        pass
+
+    def test_almost_link_but_no(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
