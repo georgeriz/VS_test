46f8105b805826f6f6d0867941015143fd767c32
Georgios Rizothanasis <trivialgeorge@gmail.com>
2018-06-05 21:06:43 +0200

    project extract links: added real tests

--- links/test_links.py
+++ links/test_links.py
@@ -3,19 +3,19 @@ from extract import foo
 
 class TestFoo(unittest.TestCase):
     def test_one_link(self):
-        pass
+        self.assertItemsEqual(foo("this is bug-123"), ["bug-123"])
     
     def test_two_links(self):
-        pass
+        self.assertItemsEqual(foo("bug-34 and bug-423 need fixing"), ["bug-34", "bug-423"])
 
     def test_punctuation_after_link(self):
-        pass
+        self.assertItemsEqual(foo("fix bug-3243."), ["bug-3243"])
 
     def test_no_link(self):
-        pass
+        self.assertItemsEqual(foo("hello world"), [])
 
     def test_almost_link_but_no(self):
-        pass
+        self.assertItemsEqual(foo("this bug-verification"), [])
 
 if __name__ == "__main__":
     unittest.main()
