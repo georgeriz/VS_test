f2ef8e6b7940dc9f0af7032bc8e623326d099c7b
Georgios Rizothanasis <trivialgeorge@gmail.com>
2018-06-06 19:47:17 +0200

    project find word: used nested function and any

--- find_word.py
+++ find_word.py
@@ -4,36 +4,20 @@ def get_next(ip, size_i, size_j):
     options = [(ip[0]+i,ip[1]+j) for i in [-1,0,1] for j in [-1,0,1]]
     return filter(lambda p: p[0]!=-1 and p[1]!=-1 and p[0]!=size_i and p[1]!=size_j, options)
 
-def foo(ip, board, word):
-    for letter in word:
-        for pp in get_next(ip, len(board), len(board[0])):
-            if board[pp[0]][pp[1]] == letter:
-                if len(word) == 1:
-                    return True
-                tmp = board[pp[0]][pp[1]]
-                board[pp[0]][pp[1]] = '-'
-                if foo(pp, board, word[1:]):
-                    return True
-                board[pp[0]][pp[1]] = tmp
-        else:
-            return False
-
-
 def find_word(board, word):
     tmp_board = copy.deepcopy(board)
-    for i, line in enumerate(tmp_board):
-        for j, char in enumerate(line):
-            if char == word[0]:
-                if len(word) == 1:
-                    return True
-                tmp = char
-                tmp_board[i][j] = '-'
-                if foo((i,j), tmp_board, word[1:]):
-                    return True
-                tmp_board[i][j] = tmp
-    return False
-
-
+    def foo(board, word, x, y):
+        if board[x][y] != word[0]: return False
+        if len(word) == 1: return True
+        tmp = board[x][y]
+        board[x][y] = "-"
+        for p in get_next((x,y), len(board), len(board[0])):
+            if foo(board, word[1:], p[0], p[1]): return True
+            
+        else: 
+            board[x][y] = tmp
+            return False
+    return any(foo(tmp_board, word, i, j) for i in range(len(board)) for j in range(len(board[0])))
 
 
 
