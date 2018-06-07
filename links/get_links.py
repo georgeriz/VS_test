53236efb703738d3e0826bccd67bcbd0669695b2
Georgios Rizothanasis <trivialgeorge@gmail.com>
2018-06-07 21:41:22 +0200

    project extract links: strip punctuation

--- links/get_links.py
+++ links/get_links.py
@@ -2,7 +2,7 @@ def get_links(msg):
     links = []
     for word in msg.split():
         if word.startswith("bug-"):
-            links.append("http://jira.com/" + word)
+            links.append("http://jira.com/" + word.strip(".,!?:"))
         elif word.startswith("cr-"):
-            links.append("goto.com/" + word)
+            links.append("goto.com/" + word.strip(".,!?:"))
     return links
