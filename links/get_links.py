66aa07f343105e482222bb13523c79bc33dc069a
Georgios Rizothanasis <trivialgeorge@gmail.com>
2018-06-07 21:28:53 +0200

    project extract links: approach with for and if

--- links/get_links.py
+++ links/get_links.py
@@ -1,2 +1,8 @@
 def get_links(msg):
-    pass
+    links = []
+    for word in msg.split():
+        if word.startswith("bug-"):
+            links.append("http://jira.com/" + word)
+        elif word.startswith("cr-"):
+            links.append("goto.com/" + word)
+    return links
