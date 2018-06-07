c607b2f1be2137a3a9180836f4c146b5c28a28bd
Georgios Rizothanasis <trivialgeorge@gmail.com>
2018-06-07 21:35:55 +0200

    project extract links: more tests

--- links/test_get_links.py
+++ links/test_get_links.py
@@ -11,5 +11,17 @@ class TestGetLinks(unittest.TestCase):
     def test_two_different_links(self):
         self.assertEqual(get_links("add bug-1234 to cr-1234-fghj"), ["http://jira.com/bug-1234", "goto.com/cr-1234-fghj"])
 
+    def test_link_at_start(self):
+        self.assertEqual(get_links("bug-800 is important"), ["http://jira.com/bug-800"])
+
+    def test_no_link(self):
+        self.assertEqual(get_links("hello world"), [])
+
+    def test_already_link(self):
+        self.assertEqual(get_links("click here goto.com/cr-rfuj-v8f9"), [])
+
+    def test_link_with_punctuation(self):
+        self.assertEqual(get_links("let's close bug-74!"), ["http://jira.com/bug-74"])
+
 if __name__ == "__main__":
     unittest.main()
