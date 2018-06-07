92b6160288dddc61d19f409cf7576f6480c12ddd
Georgios Rizothanasis <trivialgeorge@gmail.com>
2018-06-07 21:24:55 +0200

    project extract links: basic tests

--- links/test_get_links.py
+++ links/test_get_links.py
@@ -1,11 +1,15 @@
 import unittest
+from get_links import get_links
 
 class TestGetLinks(unittest.TestCase):
     def test_one_bug_link(self):
-        self.fail()
+        self.assertEqual(get_links("this is bug-12"), ["http://jira.com/bug-12"])
 
     def test_one_cr_link(self):
-        self.fail()
+        self.assertEqual(get_links("check cr-abc1-d3rt"), ["goto.com/cr-abc1-d3rt"])
+
+    def test_two_different_links(self):
+        self.assertEqual(get_links("add bug-1234 to cr-1234-fghj"), ["http://jira.com/bug-1234", "goto.com/cr-1234-fghj"])
 
 if __name__ == "__main__":
     unittest.main()
