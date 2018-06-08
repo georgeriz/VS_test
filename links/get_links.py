d2d0998c42d6f2053f2feeeb762de9dd88f892f7
Georgios Rizothanasis <trivialgeorge@gmail.com>
2018-06-08 23:21:23 +0200

    project extract links: fix for already existing link

--- links/get_links.py
+++ links/get_links.py
@@ -3,7 +3,7 @@ import re
 def get_links(msg):
     links = ["http://jira.com/" + match.upper() for match in re.findall(r'BUG-\d+', msg, re.IGNORECASE)]
     links.extend(["goto.com/" + match.upper() for match in re.findall(r'CR(?:-[a-z0-9]{4}){2}', msg, re.IGNORECASE)])
-    return links
+    return filter(lambda link: link.upper() not in msg.upper(), links)
 
 def get_links2(msg):
     links = []
