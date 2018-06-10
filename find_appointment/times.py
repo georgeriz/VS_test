ab2a4bd1feccd2638a912db308c6607414eaaf4a
Georgios Rizothanasis <trivialgeorge@gmail.com>
2018-06-10 21:28:41 +0200

    project find appointment: experiment with datetime range

--- find_appointment/times.py
+++ find_appointment/times.py
@@ -7,15 +7,23 @@ def foo(t, d):
     q[1] += m
     return q
 
-print foo("09:08", 125)
+#print foo("09:08", 125)
 
 def bar(a):
     return [datetime.strptime(i, "%H:%M") for i in a]
 
-print bar(['09:00', '11:30'])
+#print bar(['09:00', '11:30'])
 
 def blah(t, d):
     t = datetime.strptime(t, "%H:%M")
     return t + timedelta(minutes=d)
 
-print blah("09:08", 125)
+#print blah("09:08", 125)
+
+def trange(s,f,i):
+    while s < f:
+        yield s
+        s += timedelta(minutes=i)
+
+for i in trange(bar(["09:00"])[0], bar(["11:30"])[0], 10):
+    print i
