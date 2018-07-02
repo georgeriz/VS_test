a7b63c4e04d677450d82960e45cc7b923de45514
Georgios Rizothanasis <trivialgeorge@gmail.com>
2018-07-02 20:58:28 +0200

    project logging: a basic working solution

--- log_example/log_example.py
+++ log_example/log_example.py
@@ -1,5 +1,5 @@
 import logging
-print logging.__file__
-#logging.basicConfig(filename="example.log")
-#logging.warning("add this")
+
+logging.basicConfig(filename="example.log", format="%(asctime)s:%(levelname)s:%(message)s")
+logging.warning("add this")
 
