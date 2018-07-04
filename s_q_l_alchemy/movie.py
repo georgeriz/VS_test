4744e2c407441b0924427ef299adb4676abd970c
Georgios Rizothanasis <trivialgeorge@gmail.com>
2018-07-04 22:21:32 +0200

    project sqlalchemy: update movie schema to be in sync with db

--- s_q_l_alchemy/movie.py
+++ s_q_l_alchemy/movie.py
@@ -1,10 +1,10 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Date
 from base import Base
-from datetime import date
+
 
 class Movie(Base):
     __tablename__ = "movies"
 
     id = Column(Integer, primary_key=True)
     title = Column(String)
-
+    release_date = Column(Date)
--- s_q_l_alchemy/queries.py
+++ s_q_l_alchemy/queries.py
@@ -2,4 +2,4 @@ from base import Session
 from movie import Movie
 
 for movie in Session().query(Movie).all():
-    print movie.title
+    print movie.id, movie.title, movie.release_date
