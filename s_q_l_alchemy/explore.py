b68de38676b1a0deefd331df0455405aa9abbe9d
Georgios Rizothanasis <trivialgeorge@gmail.com>
2018-07-10 21:38:19 +0200

    project sqlalchemy: use reflection

--- s_q_l_alchemy/explore.py
+++ s_q_l_alchemy/explore.py
@@ -1,9 +1,24 @@
 from base import Session, engine
 from sqlalchemy import inspect
 
+from sqlalchemy import MetaData, Table
+
 inspector = inspect(engine)
 
 for table in inspector.get_table_names():
     print table
     for column in inspector.get_columns(table):
         print '     ', column['name']
+
+# in order to use ORM queries
+meta = MetaData()
+
+movies = Table("movies", meta)
+
+# connect Table object with databse based on introspection
+inspector.reflecttable(movies, None)
+
+# example Query
+session = Session(bind=engine)
+print session.query(movies).count()
+session.close()
