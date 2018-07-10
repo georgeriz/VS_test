e02b811de336d4d58280d38fcb46e3d97ae0ad6f
Georgios Rizothanasis <trivialgeorge@gmail.com>
2018-07-10 21:25:48 +0200

    project sqlalchemy: inspect tables and columns

--- s_q_l_alchemy/explore.py
+++ s_q_l_alchemy/explore.py
@@ -3,4 +3,7 @@ from sqlalchemy import inspect
 
 inspector = inspect(engine)
 
-print inspector.get_table_names()
+for table in inspector.get_table_names():
+    print table
+    for column in inspector.get_columns(table):
+        print '     ', column['name']
