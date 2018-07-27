41008f86a2da0b7c00190c782798d229b9b7dfb8
Georgios Rizothanasis <trivialgeorge@gmail.com>
2018-07-27 19:56:18 +0200

    project find appointment: a working solution

--- find_appointment/find_appointment.py
+++ find_appointment/find_appointment.py
@@ -4,13 +4,10 @@ def get_start_time(schedules, duration):
     s = [(datetime.strptime(x[0], "%H:%M"), datetime.strptime(x[1], "%H:%M")) for l in schedules for x in l]
     def trange(s,f,i):
         while s < f:
-            m = s + timedelta(minutes=1)
-            yield (s,m)
-            s = m
-    # TODO it should be zip(range, range)
-    for p in trange(datetime.strptime("09:00", "%H:%M"), datetime.strptime("19:00", "%H:%M"), duration):
-        #print p
-        if all(p[1] <= t[0] or p[0] >= t[1] for t in s):
-            # TODO return string no datetime
-            return p
+            yield s
+            s += timedelta(minutes=i)
+    for p in trange(datetime.strptime("09:00", "%H:%M"), datetime.strptime("19:00", "%H:%M") - timedelta(minutes=duration-1), 1):
+        e = p + timedelta(minutes=duration)
+        if all(e <= t[0] or p >= t[1] for t in s):
+            return p.strftime("%H:%M")
     return None
