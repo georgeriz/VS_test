14dd578f2c3d0a0d4baa691493b4a9167fcb8bd9
Georgios Rizothanasis <trivialgeorge@gmail.com>
2018-07-27 21:33:13 +0200

    project atoms: solve only for without parenth

--- atoms/module.py
+++ atoms/module.py
@@ -1,3 +1,22 @@
 def parse_molecule(formula):
+    formula += "!"
     d = {}
+    atom = ""
+    num = 0
+    for i in formula:
+        if i.isalpha():
+            if i.isupper():
+                if atom != "":
+                    d[atom] = num
+                    num = 0
+                atom = i
+            else:
+                atom += i
+        elif i.isdigit():
+            num = num*10 + int(i)
+        elif i == "!":
+            d[atom] = num
+    for k in d:
+        if d[k] == 0:
+            d[k] = 1
     return d
--- atoms/test_atoms.py
+++ atoms/test_atoms.py
@@ -7,7 +7,12 @@ def equals_atomically (obj1, obj2):
         if obj1[k] != obj2[k]:
             return False
     return True
-                
-assert(equals_atomically(parse_molecule("H2O"), {'H': 2, 'O' : 1}))
-assert(equals_atomically(parse_molecule("Mg(OH)2"), {'Mg': 1, 'O' : 2, 'H': 2}))
-assert(equals_atomically(parse_molecule("K4[ON(SO3)2]2"), {'K': 4,  'O': 14,  'N': 2,  'S': 4}))
+
+print parse_molecule("H2O")    
+print parse_molecule("OH")            
+print parse_molecule("MgOH45Fe")
+print parse_molecule("SO3")
+print parse_molecule("Fe12Au")
+#assert(equals_atomically(parse_molecule("H2O"), {'H': 2, 'O' : 1}))
+#assert(equals_atomically(parse_molecule("Mg(OH)2"), {'Mg': 1, 'O' : 2, 'H': 2}))
+#assert(equals_atomically(parse_molecule("K4[ON(SO3)2]2"), {'K': 4,  'O': 14,  'N': 2,  'S': 4}))
