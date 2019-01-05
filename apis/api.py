f90c47aa0bbe698c2f170e2111199eb6fa7fa78b
Georgios Rizothanasis <trivialgeorge@gmail.com>
2019-01-05 20:02:31 +0100

    apis project: main design

---
+++ apis/api.py
@@ -0,0 +1,8 @@
+def main():
+	# Grab CPI/Inflation data
+	
+	# Grab API/game platform data
+	
+	# Generate a plot/bar graph for the adjusted price data
+	
+	# Generate a CSV file to save for the adjusted price data
