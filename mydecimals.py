f5be03a38ab6f572f1ce4495407b260b130c6313
Georgios Rizothanasis <trivialgeorge@gmail.com>
2019-02-03 21:31:58 +0100

    using decimal module from standard library

--- mydecimals.py
+++ mydecimals.py
@@ -3,3 +3,10 @@ round(234, -1) # 230
 
 'value is {:0.1f}.'.format(1.2345) # 'value is 1.2.'
 format(2.34, '0.1f') # '2.3'
+
+from decimal import Decimal
+
+a = Decimal('4.2')
+b = Decimal('2.1')
+assert (a + b) == Decimal('6.3')
+# assert (4.2 + 2.1) == 6.3 would fail
