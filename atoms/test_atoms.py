6e06f23a91a91dfc67e4bd9cd40e2e44d5fe0b8b
Georgios Rizothanasis <trivialgeorge@gmail.com>
2019-02-09 22:02:46 +0100

    changed test to use the new approach

--- atoms/test_atoms.py
+++ atoms/test_atoms.py
@@ -1,4 +1,4 @@
-from module import parse_molecule
+from more_atoms import parse_molecule
 
 def equals_atomically (obj1, obj2):
     if len(obj1) != len(obj2):
@@ -8,11 +8,11 @@ def equals_atomically (obj1, obj2):
             return False
     return True
 
-print parse_molecule("H2O")    
-print parse_molecule("OH")            
-print parse_molecule("MgOH45Fe")
-print parse_molecule("SO3")
-print parse_molecule("Fe12Au")
-#assert(equals_atomically(parse_molecule("H2O"), {'H': 2, 'O' : 1}))
-#assert(equals_atomically(parse_molecule("Mg(OH)2"), {'Mg': 1, 'O' : 2, 'H': 2}))
-#assert(equals_atomically(parse_molecule("K4[ON(SO3)2]2"), {'K': 4,  'O': 14,  'N': 2,  'S': 4}))
+#print parse_molecule("H2O")    
+#print parse_molecule("OH")            
+#print parse_molecule("MgOH45Fe")
+#print parse_molecule("SO3")
+#print parse_molecule("Fe12Au")
+assert(equals_atomically(parse_molecule("H2O"), {'H': 2, 'O' : 1}))
+assert(equals_atomically(parse_molecule("Mg(OH)2"), {'Mg': 1, 'O' : 2, 'H': 2}))
+assert(equals_atomically(parse_molecule("K4[ON(SO3)2]2"), {'K': 4,  'O': 14,  'N': 2,  'S': 4}))
