706a19d283bf1579b18ed4631525f3d560d4b10e
Georgios Rizothanasis <trivialgeorge@gmail.com>
2019-02-26 21:10:38 +0100

    numpy index slicing

--- some_numpy.py
+++ some_numpy.py
@@ -3,3 +3,10 @@ import numpy as np
 grid = np.zeros(shape=(10000,10000), dtype=float)
 
 grid += 10
+
+a = np.array([[1,2,3],[4,5,6],[7,8,9]])
+
+print(a[1:3,1:3])
+
+b = np.where(a<5, 0, a)
+print(b) # [[0,0,0],[0,5,6],[7,8,9]]
