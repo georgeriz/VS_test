2f607944150caf54e94136d5721b7aa120c91098
Georgios Rizothanasis <trivialgeorge@gmail.com>
2019-03-04 00:00:06 +0100

    override fixture

--- customer_tests/test_customers.py
+++ customer_tests/test_customers.py
@@ -1,7 +1,13 @@
+import pytest
+
 def test_customer_records(make_customer_record):
     customer_1 = make_customer_record("Lisa")
     customer_2 = make_customer_record("Mike")
     customer_3 = make_customer_record("Meredith")
 	
+@pytest.fixture
+def customer_repr():
+	return 'customer_Maria'
+	
 def test_customer_repr(customer, customer_repr):
-	assert customer_repr == customer.name
+	assert customer_repr == 'customer_' + customer.name
