777a69b272d269f1d0a9e9aaa01a87a5220328a0
Georgios Rizothanasis <trivialgeorge@gmail.com>
2019-03-03 23:48:01 +0100

    move fixtures to conftest

---
+++ customer_tests/conftest.py
@@ -0,0 +1,19 @@
+import pytest
+
+from Customer import Customer
+
+@pytest.fixture
+def make_customer_record():
+
+	created_records = []
+	
+	def _make_customer_record(name):
+		record = Customer(name, [])
+		created_records.append(record)
+		return record
+
+	yield _make_customer_record
+	
+	for record in created_records:
+		# clean up code
+		record.destroy()
--- customer_tests/test_customers.py
+++ customer_tests/test_customers.py
@@ -1,24 +1,3 @@
-import pytest
-
-from Customer import Customer
-
-@pytest.fixture
-def make_customer_record():
-
-	created_records = []
-	
-	def _make_customer_record(name):
-		record = Customer(name, [])
-		created_records.append(record)
-		return record
-
-	yield _make_customer_record
-	
-	for record in created_records:
-		# clean up code
-		record.destroy()
-		
-		
 def test_customer_records(make_customer_record):
     customer_1 = make_customer_record("Lisa")
     customer_2 = make_customer_record("Mike")
