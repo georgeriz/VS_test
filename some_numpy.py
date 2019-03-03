3f8289cb571470ca7fb1ca961dce984c1f4babec
Georgios Rizothanasis <trivialgeorge@gmail.com>
2019-03-03 23:09:00 +0100

    linear algebra

--- some_numpy.py
+++ some_numpy.py
@@ -10,3 +10,9 @@ print(a[1:3,1:3])
 
 b = np.where(a<5, 0, a)
 print(b) # [[0,0,0],[0,5,6],[7,8,9]]
+
+# matrix: follows linear algebra rules of computation
+m = np.matrix([[1,-2,3],[0,4,5],[7,8,-9]])
+v = np.matrix([[2],[3],[4]])
+
+print(m*v)
