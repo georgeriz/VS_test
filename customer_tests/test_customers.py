5047ba3cacf14bf5fc2c5c7cd85753f4514dacca
Georgios Rizothanasis <trivialgeorge@gmail.com>
2019-03-03 23:34:20 +0100

    Revert "manage data created by factory as fixture"
    This reverts commit 44f00ee1f8491ee5af7b60b3e2da948d9547b825.

--- customer_tests/test_customers.py
+++ customer_tests/test_customers.py
@@ -5,14 +5,9 @@ from Customer import Customer
 @pytest.fixture
 def make_customer_record():
 	
-	created_records = []
-	
 	def _make_customer_record(name):
-		record = Customer(name, [])
-		created_records.append(record)
-		print(created_records)
-		return record
-	
+		return Customer(name, [])
+		
 	return _make_customer_record
 		
 def test_customer_records(make_customer_record):
