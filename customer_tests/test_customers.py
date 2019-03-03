d6d74619fa7e8ff212edf9b7aca2c86f1875934f
Georgios Rizothanasis <trivialgeorge@gmail.com>
2019-03-03 23:37:26 +0100

    manage data with factory as fixture

--- customer_tests/test_customers.py
+++ customer_tests/test_customers.py
@@ -4,11 +4,16 @@ from Customer import Customer
 
 @pytest.fixture
 def make_customer_record():
+
+	created_records = []
 	
 	def _make_customer_record(name):
-		return Customer(name, [])
-		
+		record = Customer(name, [])
+		created_records.append(record)
+		return record
+
 	return _make_customer_record
+
 		
 def test_customer_records(make_customer_record):
     customer_1 = make_customer_record("Lisa")
