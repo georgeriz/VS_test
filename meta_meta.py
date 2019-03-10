06aebd08f50daaff9b6eb5652aac02a0441a2f3f
Georgios Rizothanasis <trivialgeorge@gmail.com>
2019-03-10 21:04:42 +0100

    added time reporting decorator

--- meta_meta.py
+++ meta_meta.py
@@ -1,6 +1,23 @@
+import time
+
+def timethis(func):
+	'''
+	Decorator that reports the execution timethis
+	'''
+	def wrapper(*args, **kwargs):
+		start = time.time()
+		result = func(*args, **kwargs)
+		end = time.time()
+		print(func.__name__, end - start)
+		return result
+	return wrapper
+
+@timethis
 def countdown(n):
 	'''
 	Counts down
 	'''
 	while n > 0:
 		n -= 1
+		
+countdown(100000)
