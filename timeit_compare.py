d4cec1df710a0a3b8d44683a6793ea4314d488cd
georgeriz <georgeriz@users.noreply.github.com>
2019-04-03 17:00:23 +0200

    compare two different 'find duplicates' approaches

---
+++ timeit_compare.py
@@ -0,0 +1,34 @@
+def timeit_decorator(original_func):
+    def new_func():
+        from timeit import timeit
+        print(original_func.__name__, timeit(original_func, number=10000))
+    return new_func
+
+def get_items():
+    for i in range(1000):
+        yield i
+
+def transform(n):
+    return str(n)
+
+@timeit_decorator
+def foo():
+    names = set()
+    duplicates = []
+    for data in get_items():
+        name = transform(data)
+        if name in names:
+            duplicates.append(name)
+        names.add(name)
+    return duplicates
+
+@timeit_decorator
+def bar():
+    from collections import Counter
+    names = Counter(map(transform, get_items()))
+    duplicates = [k for k, c in names.items() if c > 1]
+    return duplicates
+
+
+foo()
+bar()
